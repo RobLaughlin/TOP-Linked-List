f09f03043adba293e81dca4850d8c2fde2fa8896
Rob Laughlin <waterfallkicks@gmail.com>
2025-01-09 16:21:58 -0800

    Implement basic linked list data structure

--- app.js
+++ app.js
@@ -1,4 +1,50 @@
 import { LinkedList } from "./linkedlist.js";
 
-const ll = new LinkedList();
-console.log(ll.toString());
+function printLinkedListStatus(linkedlist) {
+    console.log(`Linked list: ${linkedlist.toString()}`);
+    console.log(`Size: ${linkedlist.size()}\n`);
+}
+
+function testLinkedList(ll, numTests) {
+    for (let i = 0; i < numTests; i++) {
+        ll.append(i);
+        printLinkedListStatus(ll);
+    }
+
+    for (let i = -numTests; i < 0; i++) {
+        ll.prepend(i);
+        printLinkedListStatus(ll);
+    }
+
+    for (let i = 0; i < 2 * numTests; i++) {
+        console.log(`Value at index: ${i}: ${ll.at(i).value}`);
+    }
+
+    const foundTests = [-1, -numTests, 0, numTests - 1, numTests];
+    foundTests.forEach((test) => {
+        console.log(`There is a ${test} at index: ${ll.find(test)}`);
+    });
+
+    console.log(`Inserting ${"A"} at index 0...`);
+    ll.insertAt("A", 0);
+    printLinkedListStatus(ll);
+
+    console.log(`Inserting ${"B"} at index ${numTests}...`);
+    ll.insertAt("B", numTests);
+    printLinkedListStatus(ll);
+
+    console.log(`Inserting ${"C"} at index ${2 * numTests - 1}...`);
+    ll.insertAt("C", 2 * numTests - 1);
+    printLinkedListStatus(ll);
+}
+
+function init() {
+    const NUM_TESTS = 5;
+
+    const ll = new LinkedList();
+    printLinkedListStatus(ll);
+
+    testLinkedList(ll, NUM_TESTS);
+}
+
+init();
--- linkedlist.js
+++ linkedlist.js
@@ -1,13 +1,7 @@
 export class Node {
-    #nextNode;
-
-    Node(value = null, nextNode = null) {
+    constructor(value = null, nextNode = null) {
         this.value = value;
-        this.#nextNode = nextNode;
-    }
-
-    nextNode() {
-        return this.#nextNode;
+        this.nextNode = nextNode;
     }
 }
 
@@ -16,14 +10,118 @@ export class LinkedList {
     #tail = null;
     #size = 0;
 
-    LinkedList() {}
+    constructor() {}
+
+    size() {
+        return this.#size;
+    }
+
+    append(value) {
+        // console.log(value);
+        if (this.#size === 0) {
+            this.#head = new Node(value, null);
+            this.#tail = this.#head;
+        } else {
+            this.#tail.nextNode = new Node(value);
+            this.#tail = this.#tail.nextNode;
+        }
+        this.#size++;
+    }
+
+    prepend(value) {
+        if (this.size() === 0) {
+            this.#head = new Node(value, null);
+            this.#tail = this.#head;
+        } else {
+            const oldHead = new Node(this.#head.value, this.#head.nextNode);
+            this.#head = new Node(value, oldHead);
+        }
+        this.#size++;
+    }
+
+    head() {
+        return this.#head;
+    }
+
+    tail() {
+        return this.#tail;
+    }
+
+    at(index) {
+        if (index >= this.#size) {
+            throw new RangeError(
+                `Index (${index}) must be strictly less than the size of the linked list (${
+                    this.#size
+                })`
+            );
+        }
+
+        let i = 0;
+        let curNode = this.#head;
+        while (i < index) {
+            curNode = curNode.nextNode;
+            i++;
+        }
+
+        return curNode;
+    }
+
+    contains(value) {
+        let curNode = this.#head;
+        while (curNode !== null) {
+            if (curNode.value === value) {
+                return true;
+            }
+            curNode = curNode.nextNode;
+        }
+
+        return false;
+    }
+
+    find(value) {
+        let curNode = this.#head;
+        let i = 0;
+        while (curNode !== null) {
+            if (curNode.value === value) {
+                return i;
+            }
+            curNode = curNode.nextNode;
+            i++;
+        }
+
+        return null;
+    }
+
+    insertAt(value, index) {
+        if (index >= this.#size || index < 0) {
+            throw new RangeError(
+                `Index (${index}) must be strictly less than the size of the linked list (${
+                    this.#size
+                }) and strictly greater than 0.`
+            );
+        }
+
+        const newNode = new Node(value);
+        if (index === 0) {
+            newNode.nextNode = this.#head;
+            this.#head = newNode;
+        } else if (index === this.#size - 1) {
+            this.#tail.nextNode = newNode;
+            this.#tail = newNode;
+        } else {
+            const prevNode = this.at(index - 1);
+            const newNextNode = prevNode.nextNode;
+            prevNode.nextNode = newNode;
+            newNode.nextNode = newNextNode;
+        }
+    }
 
     toString() {
         let curNode = this.#head;
         let llStr = "";
         while (curNode !== null) {
-            llStr += `(${curNode.value.toString()}) -> `;
-            curNode = curNode.nextNode();
+            llStr += `(${curNode.value}) -> `;
+            curNode = curNode.nextNode;
         }
 
         llStr += "null";
